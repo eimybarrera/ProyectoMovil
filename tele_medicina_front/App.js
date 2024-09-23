df5dc52b8dbc2573cc679949b3757914b3751797
eimybarrera <eimybarrera2019@gmail.com>
2024-09-22 21:54:07 -0500

    [main][primer commit]

---
+++ tele_medicina_front/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Este es nuestro proyecto dee telemedicinaaa</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
